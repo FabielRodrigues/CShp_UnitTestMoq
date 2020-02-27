f4ce9312029932a595c82224378cf3fd69705202
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2020-02-27 13:33:25 -0300

    Somar dois números.

--- CShp_XUnitTest/UnitTest1.cs
+++ CShp_XUnitTest/UnitTest1.cs
@@ -1,5 +1,5 @@
 using CShp_UnitTestMoq;
-using System;
+using Moq;
 using Xunit;
 
 namespace CShp_XUnitTest
@@ -11,6 +11,13 @@ namespace CShp_XUnitTest
         {
             // Arrange
             Moq.Mock<ICalculadora> mock = new Moq.Mock<ICalculadora>();
+            mock.Setup(x => x.Calcular(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>())).Returns(("somar", 7.7));
+            MaquinaCalculadora maqCalc = new MaquinaCalculadora(mock.Object);
+            // Act
+            (string operacao, double resultado) op = maqCalc.Calcular("somar", 3.2, 4.5);
+            // Assert
+            Assert.Equal("somar", op.operacao);
+            Assert.Equal(7.7, op.resultado);
         }
     }
 }
