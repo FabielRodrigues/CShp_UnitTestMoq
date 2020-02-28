2acdd9a41a80b30a9f451d367a29e539aa35198c
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2020-02-28 08:52:51 -0300

    Multiplicar e Dividir - OK.

--- CShp_XUnitTest/UnitTest1.cs
+++ CShp_XUnitTest/UnitTest1.cs
@@ -33,5 +33,33 @@ namespace CShp_XUnitTest
             Assert.Equal("subtrair", op.operacao);
             Assert.Equal(-1.3, op.resultado);
         }
+
+        [Fact]
+        public void Multiplicar_Dois_Numeros()
+        {
+            // Arrange
+            Moq.Mock<ICalculadora> mock = new Moq.Mock<ICalculadora>();
+            mock.Setup(x => x.Calcular(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>())).Returns(("multiplicar", 14.4));
+            MaquinaCalculadora maqCalc = new MaquinaCalculadora(mock.Object);
+            // Act
+            (string operacao, double resultado) op = maqCalc.Calcular("multiplicar", 3.2, 4.5);
+            // Assert
+            Assert.Equal("multiplicar", op.operacao);
+            Assert.Equal(14.4, op.resultado);
+        }
+
+        [Fact]
+        public void Dividir_Dois_Numeros()
+        {
+            // Arrange
+            Moq.Mock<ICalculadora> mock = new Moq.Mock<ICalculadora>();
+            mock.Setup(x => x.Calcular(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>())).Returns(("dividir", 0.71));
+            MaquinaCalculadora maqCalc = new MaquinaCalculadora(mock.Object);
+            // Act
+            (string operacao, double resultado) op = maqCalc.Calcular("multiplicar", 3.2, 4.5);
+            // Assert
+            Assert.Equal("dividir", op.operacao);
+            Assert.Equal(0.71, op.resultado);
+        }
     }
 }
